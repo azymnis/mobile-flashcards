70d31cd4246eb916d26c8a6fbbce48e7ec09c60f
Argyris Zymnis <argyris.zymnis@zee.aero>
2018-01-04 13:35:29 -0800

    Created api file

---
+++ utils/api.js
@@ -0,0 +1,46 @@
+import { AsyncStorage } from 'react-native'
+
+const MOBILE_FLASHCARDS_KEY = "mobile-flashcards:data"
+
+/**
+ * Return all of the decks along with their titles, questions, and answers.
+ */
+export function getDecks() {
+  return AsyncStorage.getItem(MOBILE_FLASHCARDS_KEY)
+}
+
+/**
+ * Take in a single title argument and return the deck associated with that id.
+ */
+export function getDeck(title) {
+  return AsyncStorage.getItem(MOBILE_FLASHCARDS_KEY)
+    .then(results => {
+      const data = JSON.parse(results)
+      return data[title]
+    })
+}
+
+/**
+ * Take in a single title argument and add it to the decks.
+ */
+export function saveDeckTitle(title) {
+  return AsyncStorage.mergeItem(MOBILE_FLASHCARDS_KEY, JSON.stringify({
+    [title]: {
+      title: title,
+      questions: []
+    }
+  }))
+}
+
+/**
+ * Take in two arguments, title and card, and will add the card to
+ * the list of questions for the deck with the associated title.
+ */
+export function addCardToDeck(title, card) {
+  return AsyncStorage.getItem(MOBILE_FLASHCARDS_KEY)
+    .then(results => {
+      const data = JSON.parse(results)
+      data[title].questions.push(card)
+      AsyncStorage.setItem(MOBILE_FLASHCARDS_KEY, JSON.stringify(data))
+    })
+}
